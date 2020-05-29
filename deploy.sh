600d1242f665407507a61a14ca5fc26d4eede158
Pavel Makarenko <cryfall@gmail.com>
2020-05-29 19:11:28 +0300

    added title

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,13 @@
 set -e
 
 # build
-export NODE_ENV=production
 npm run build
 
 # navigate into the build output directory
 cd dist
 
 # if you are deploying to a custom domain
-echo 'swaps.app' > CNAME
+echo 'www.swaps.app' > CNAME
 
 git init
 git add -A
